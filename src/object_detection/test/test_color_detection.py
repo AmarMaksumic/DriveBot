49cd67e0f7d61019a280863bb8701251257c2747
Amar Maksumić <amar.maksumich@gmail.com>
2025-03-26 14:13:06 -0400

    new color values

--- src/object_detection/test/test_color_detection.py
+++ src/object_detection/test/test_color_detection.py
@@ -2,16 +2,23 @@ import cv2
 import numpy as np
 
 # read image 
-rgb_image = cv2.imread('color_test.jpg')
+rgb_image = cv2.imread('camera.png')
 # to hsv
 hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
 # bounds
-blue_low = np.array([110,50,50])
-blue_high = np.array([130,255,255])
+low_1 = np.array([100,140,140])
+high_1 = np.array([140,255,255])
+
+# low_2 = np.array([160,144,190])
+# high_2 = np.array([180,179,255])
 # hsv threshold
-blue_mask = cv2.inRange(hsv_image, blue_low, blue_high)
+mask_1 = cv2.inRange(hsv_image, low_1, high_1)
+# mask_2 = cv2.inRange(hsv_image, low_2, high_2)
+mask = mask_1 #+ mask_2
+cv2.imshow('mask', mask)
+cv2.waitKey(0)
 # find largest contour
-contours, _ = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 if len(contours) > 0:
     largest_contour = max(contours, key=cv2.contourArea)
     x, y, w, h = cv2.boundingRect(largest_contour)
@@ -19,5 +26,5 @@ if len(contours) > 0:
     cv2.rectangle(rgb_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
 # show image
-cv2.imshow('blue_mask', rgb_image)
+cv2.imshow('filtered_mask', rgb_image)
 cv2.waitKey(0)
